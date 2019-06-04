1000999a2a0305fbb9a6d9e4699656e80891964a
Daniel-Wh <whitneyd@southwestern.edu>
2019-06-04 09:55:51 -0500

    added cascading dropdown - pending http requests

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -11,7 +11,20 @@ export class DropdownComponent implements OnInit {
       name: 'all stations', id: 0
     },
     { name: 'Lake Charles', id: 1},
-    { name: 'El Paso', id: 2 }
+    { name: 'El Paso', id: 2 },
+    { name: 'Midland', id: 3 },
+    { name: 'Del Rio', id: 4 },
+    { name: 'Corpus Christi', id: 5 },
+    { name: 'Lake Charles', id: 6 },
+    { name: 'Slidell', id: 7 },
+    { name: 'Jackson', id: 8 },
+    { name: 'Shelby', id: 9 },
+    { name: 'Tallahassee', id: 10 },
+    { name: 'Miami', id: 11 },
+    { name: 'Tampa', id: 12 },
+    { name: 'Jacksonville', id: 13 },
+    { name: 'Key West', id: 14 },
+    { name: 'Brownsville', id: 15 }
   ];
   seasons = [
     {name: 'all seasons', id: 100},
