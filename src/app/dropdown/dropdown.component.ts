38a95623178e4bde610c5bc61637f23720a4ec56
Daniel-Wh <whitneyd@southwestern.edu>
2019-06-05 11:25:04 -0500

    + event binding - updated styling

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dropdown.component.css']
 })
 export class DropdownComponent implements OnInit {
+
+  public selected_station = 0;
+  public selected_season = 0;
+  public selected_oni = 0;
+
   stations = [
-    {
-      name: 'all stations', id: 0
-    },
+    {name: 'Select Station', id: 99},
+    {name: 'All Stations', id: 0},
     { name: 'Lake Charles', id: 1},
     { name: 'El Paso', id: 2 },
     { name: 'Midland', id: 3 },
@@ -27,6 +31,7 @@ export class DropdownComponent implements OnInit {
     { name: 'Brownsville', id: 15 }
   ];
   seasons = [
+    {name: 'Select Season', id: 99},
     {name: 'all seasons', id: 100},
     {name: 'Spring', id: 101},
     {name: 'Summer', id: 102},
@@ -34,11 +39,26 @@ export class DropdownComponent implements OnInit {
     {name: 'Winter', id: 104}
   ];
   oni = [
+    {option: "Oceanic Nino Index", id: 99 },
     {option: 'No ONI specification', id: 200},
     {option: 'El Nino', id: 201},
     {option: 'La Nina', id: 202},
     {option: 'Neutral', id: 203}
   ];
+
+  onSelectStation(station_id: number) {
+    this.selected_station = station_id;
+    console.log(this.selected_station)
+  }
+
+  onSelectSeason(season_id: number) {
+    this.selected_season = season_id;
+    console.log(this.selected_season)
+  }
+  onSelectOni(oni_id: number) {
+    this.selected_oni = oni_id;
+    console.log(this.selected_oni)
+  }
 
   constructor() { }
 
